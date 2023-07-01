f1dbeff885f425eed3c3727d8cf40896621501b7
Vipul <vipulkumar.kansar@gmail.com>
2023-07-01 09:27:59 +0300

    Commit

---
+++ src/main/java/co/sda/carentalproject/CaRentalProjectApplication.java
@@ -0,0 +1,13 @@
+package co.sda.carentalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaRentalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaRentalProjectApplication.class, args);
+    }
+
+}
